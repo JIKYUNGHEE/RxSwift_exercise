d557aa9a5dc1fee072f7d23d9c10110465630813
JIKYUNGHEE <wlrudgml517@gmail.com>
2021-04-02 17:06:20 +0900

    RxSwift 야곰 step3

---
+++ step3+Empty/RxSwift+MVVM/Pages/Menu.swift
@@ -0,0 +1,14 @@
+//
+//  Menu.swift
+//  RxSwift+MVVM
+//
+//  Created by 지경희 on 2021/04/02.
+//  Copyright © 2021 iamchiwon. All rights reserved.
+//
+
+import Foundation
+
+// ViewModel
+struct Menu {
+    
+}
